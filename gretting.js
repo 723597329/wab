2019fed741eebac597a77b03ff771160aa0f4f69
723597329 <43269640+723597329@users.noreply.github.com>
2018-11-24 22:42:58 +0900

    Add files via upload

---
+++ gretting.js
@@ -0,0 +1,37 @@
+let form=document.querySelector('.js-form');
+let jsinput=form.querySelector("input");
+let greeting=document.querySelector(".js-greeting");
+
+function handle_submit(event){
+	event.preventDefault();
+	const currentValue=jsinput.value;
+	localStorage.setItem("user_name",currentValue);
+	paint_greeting(currentValue);
+}
+
+function ask_name(){
+	form.classList.add("showing");
+	form.addEventListener("submit",handle_submit);
+
+}
+
+
+function paint_greeting(text){
+	form.classList.remove("showing");
+	greeting.classList.add("showing");
+	greeting.innerText=`안녕하세요 ${text}님`;
+}
+
+function road_name()
+{
+	const current_user=localStorage.getItem('user_name');
+	if(current_user===null){
+		ask_name();
+	}else{
+		paint_greeting(current_user);
+	}
+}
+function init(){
+	road_name();
+}
+init();
