ec13104b136207932aab296237ac7813a043362d
723597329 <a3329861@naver.com>
2018-11-28 20:49:24 +0900

    php부분삭제

--- create_prosess.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<?php
-echo 1;
-echo $_POST['name'];
-echo $_POST['title'];
-echo $_POST['description'];
-file_put_contents('data/'.$_POST['title'],$_POST['description']);
-header('Location: /hellophp/hellophp.php?id='.$_POST['title']);
-?>
